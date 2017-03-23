0538d9948702d4a1bc25853ed618c9adb939377f
weissekat <weissekat@gmail.com>
2017-03-23 08:26:37 +0200

    Bluetooth enable added, fixed bug for fast rotation

--- main.c
+++ main.c
@@ -1,12 +1,17 @@
 #define CLOCK 9600000
 
 /*
+ATTINY13A
+
+
 IAR compiler!
-- PB0 is connected to SDA via 330 ohm resistor while CPU via 1k resistor,
+- PB0 (5) is connected to SDA via 330 ohm resistor while CPU via 1k resistor,
       such a voltage divider helps to set 1 on the line
-- PB1 is connected to SCL directly
-- PB2 is not connected, debug info
-- PB3 is connected to "NEWS" button, its wires are cut off
+- PB1 (6) is connected to SCL directly
+- PB2 (7) is not connected, debug info
+- PB3 (2) is connected to "NEWS" button, its wires are cut off
+- PB4 (3) is connected to transistor 2n3906 used for Bluetooth module enable
+      high level is OFF, low level is ON
 */
 
 #define   ENABLE_BIT_DEFINITIONS
@@ -55,14 +60,20 @@ void main(void) {
     DDRB |= (1 << PB2);
     PORTB &= ~(1 << PB2);
 
+    DDRB |= (1 << PB4);
+    PORTB |= (1 << PB4);
+
     while(1) {
         waiting_for_start:
             if ((PINB&(1<<PB3))==0x00) {
                 __delay_cycles(CLOCK/1000*300);
-                if (mode)
+                if (mode) {
                     mode = 0;
-                else
+                    PORTB |= (1 << PB4);
+                } else {
                     mode = 1;
+                    PORTB &= ~(1 << PB4);
+                }
             }
 
             if (mode) {
@@ -77,6 +88,7 @@ void main(void) {
             goto waiting_for_start;
 
         capturing_data:
+            // catch 140 (8C)
             if (check1()) goto waiting_for_start; // 1
             if (check0()) goto waiting_for_start; // 0
             if (check0()) goto waiting_for_start; // 0
@@ -88,6 +100,7 @@ void main(void) {
 
             if (check0()) goto waiting_for_start; // 0, ACK
 
+            // catch 80 (50)
             if (check0()) goto waiting_for_start; // 0
             if (check1()) goto waiting_for_start; // 1
             if (check0()) goto waiting_for_start; // 0
@@ -98,21 +111,26 @@ void main(void) {
             if (check0()) goto waiting_for_start; // 0
 
             if (check0()) goto waiting_for_start; // 0, ACK
+
+            // catch & set 209 (D1)
             if (check1()) goto waiting_for_start; // 1
             if (check1()) goto waiting_for_start; // 1
             if (check0()) goto waiting_for_start; // 0
             if (check1()) goto waiting_for_start; // 1
             if (check0()) goto waiting_for_start; // 0
+
+            // set 0, enable output
+            PORTB &= ~(1 << PB0);
+            DDRB |= (1 << PB0);
+
+            // wait for 1 rising edge
+            GIFR |= (1 << INTF0); while (!(GIFR & (1 << INTF0)));
 
             // debug info: started
             PORTB ^= ( 1<<PB2 ); PORTB ^= ( 1<<PB2 );
 
             // interrupt on falling edge
             MCUCR &= ~0x03; MCUCR |= 0x02;
-
-            // set 0, enable output
-            PORTB & = ~(1 << PB0);
-            DDRB |= (1 << PB0);
 
             // wait 2 tacts
             GIFR |= (1 << INTF0); while (!(GIFR & (1 << INTF0)));
